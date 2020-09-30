4d08cdff20ae1d65bf9a45a36a6014515f57eebf
adminwillie <jwilliam@sheetz.com>
2020-09-30 14:20:08 -0400

    Initial write

---
+++ Poshbot.vSphere/Public/Get-PBContentLibrary.ps1
@@ -0,0 +1,35 @@
+function Get-PBContentLibrary {
+    <#
+    .SYNOPSIS
+        PoshBot command retrieves the content libraries available on a vCenter Server system.
+    .EXAMPLE
+        !getcontentlibrary library1
+    #>
+    [PoshBot.BotCommand(CommandName = 'getcontentlibrary')]
+    [cmdletbinding()]
+    param(
+        [PoshBot.FromConfig()]
+        [parameter(Mandatory)]
+        [hashtable]$Connection,
+        [parameter(Position = 0, Mandatory = $false, ValueFromRemainingArguments = $true)]
+        [string[]]$library
+    )
+
+    #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+    $creds = [pscredential]::new($Connection.Username, ($Connection.Password | ConvertTo-SecureString -AsPlainText -Force))
+    $null = Connect-VIServer -Server $Connection.Server -Credential $creds
+    if ($library) {
+        $objects = Get-ContentLibrary $library
+    }
+    else {
+        $objects = Get-ContentLibrary | select Name
+    }
+    
+    $ResponseSplat = @{
+        Text = Format-PBvSphereObject -Object $objects -FunctionName $MyInvocation.MyCommand.Name
+        AsCode = $true
+    }
+
+    Disconnect-Viserver -Confirm:$false
+    New-PoshBotTextResponse @ResponseSplat
+}
