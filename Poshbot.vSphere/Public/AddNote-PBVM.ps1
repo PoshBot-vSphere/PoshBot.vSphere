cae27b3d538db76eeb661f0365452296eb5b9ede
Surabhi Tandon <tandons@vmware.com>
2020-09-30 17:49:38 -0400

    Add note to VM with current date

---
+++ Poshbot.vSphere/Public/AddNote-PBVM.ps1
@@ -0,0 +1,35 @@
+function Start-PBVM {
+    <#
+    .SYNOPSIS
+        PoshBot command to start VM.
+    .EXAMPLE
+        !startvm vm1
+    #>
+    [PoshBot.BotCommand(CommandName = 'startvm')]
+    [cmdletbinding()]
+    param(
+        [PoshBot.FromConfig()]
+        [parameter(Mandatory)]
+        [hashtable]$Connection,
+        [parameter(Position = 0, Mandatory = $true, ValueFromRemainingArguments = $true)]
+        [string[]]$vm
+    )
+
+    #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+    $creds = [pscredential]::new($Connection.Username, ($Connection.Password | ConvertTo-SecureString -AsPlainText -Force))
+    $null = Connect-VIServer -Server $Connection.Server -Credential $creds
+    if ($vm) {
+        $objects = Get-VM fritz-001 | Set-VM -Notes "last updated $(Get-Date)" -confirm:$false
+    }
+    else {
+        exit
+    }
+
+    $ResponseSplat = @{
+        Text = Format-PBvSphereObject -Object $objects -FunctionName $MyInvocation.MyCommand.Name
+        AsCode = $true
+    }
+
+    Disconnect-Viserver -Confirm:$false
+    New-PoshBotTextResponse @ResponseSplat
+}
