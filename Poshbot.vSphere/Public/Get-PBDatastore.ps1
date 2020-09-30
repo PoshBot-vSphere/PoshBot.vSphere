028cfca9f01cc8572cc985ecdb6546af0dec15dc
adminwillie <jwilliam@sheetz.com>
2020-09-30 15:22:46 -0400

    Initial write

---
+++ Poshbot.vSphere/Public/Get-PBDatastore.ps1
@@ -0,0 +1,35 @@
+function Get-PBDatastore {
+    <#
+    .SYNOPSIS
+        PoshBot command retrieves the datastores available on a vCenter Server system.
+    .EXAMPLE
+        !getdatastore DS1
+    #>
+    [PoshBot.BotCommand(CommandName = 'getdatastore')]
+    [cmdletbinding()]
+    param(
+        [PoshBot.FromConfig()]
+        [parameter(Mandatory)]
+        [hashtable]$Connection,
+        [parameter(Position = 0, Mandatory = $false, ValueFromRemainingArguments = $true)]
+        [string[]]$ds
+    )
+
+    #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+    $creds = [pscredential]::new($Connection.Username, ($Connection.Password | ConvertTo-SecureString -AsPlainText -Force))
+    $null = Connect-VIServer -Server $Connection.Server -Credential $creds
+    if ($ds) {
+        $objects = Get-Datastore $ds
+    }
+    else {
+        $objects = Get-Datastore | select Name | ft
+    }
+    
+    $ResponseSplat = @{
+        Text = Format-PBvSphereObject -Object $objects -FunctionName $MyInvocation.MyCommand.Name
+        AsCode = $true
+    }
+
+    Disconnect-Viserver -Confirm:$false
+    New-PoshBotTextResponse @ResponseSplat
+}
