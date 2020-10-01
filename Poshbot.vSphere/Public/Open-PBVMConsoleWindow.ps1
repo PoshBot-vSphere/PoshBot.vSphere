a88b531f416c9b89b63c31546ccf5fd6555da327
adminwillie <jwilliam@sheetz.com>
2020-10-01 14:26:57 -0400

    Initial write

---
+++ Poshbot.vSphere/Public/Open-PBVMConsoleWindow.ps1
@@ -0,0 +1,35 @@
+function Open-PBVMConsoleWindow {
+    <#
+    .SYNOPSIS
+        PoshBot command to open console window to specified VM.
+    .EXAMPLE
+        !openvmconsole vm1
+    #>
+    [PoshBot.BotCommand(CommandName = 'openvmconsole')]
+    [cmdletbinding()]
+    param(
+        [PoshBot.FromConfig()]
+        [parameter(Mandatory)]
+        [hashtable]$Connection,
+        [parameter(Position = 0, Mandatory = $true, ValueFromRemainingArguments = $true)]
+        [string[]]$vm
+    )
+
+    #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+    $creds = [pscredential]::new($Connection.Username, ($Connection.Password | ConvertTo-SecureString -AsPlainText -Force))
+    $null = Connect-VIServer -Server $Connection.Server -Credential $creds
+    if ($vm) {
+        $objects = Get-VM $vm | Open-VMConsoleWindow -UrlOnly
+    }
+    else {
+        Exit
+    }
+    
+    $ResponseSplat = @{
+        Text = Format-PBvSphereObject -Object $objects -FunctionName $MyInvocation.MyCommand.Name
+        AsCode = $true
+    }
+
+    Disconnect-Viserver -Confirm:$false
+    New-PoshBotTextResponse @ResponseSplat
+}
